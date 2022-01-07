0a89035b219955036b0caf8f694111ae445d1d19
marianela-miguel3 <nelaecopol@gmail.com>
2022-01-06 21:21:28 -0300

    cambios-nela

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,27 +6,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TramoModule } from './tramo/tramo.module';
-// <<<<<<< create-artefacto
-// import { ArtefactoController } from './artefacto/artefacto.controller';
-// import { ArtefactoService } from './artefacto/artefacto.service';
 import { ArtefactoModule } from './artefacto/artefacto.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { TramoaccesorioModule } from './tramoaccesorio/tramoaccesorio.module';
 @Module({
   imports:[ServeStaticModule.forRoot({rootPath:join(__dirname,"..","app"),
-}),TypeOrmModule.forRoot(),AccesoriosModule, ArtefactoModule, TramoModule],
-  controllers: [AppController
-    // , ArtefactoController
-  ],
-  providers: [AppService
-    // , ArtefactoService
-  ],
-// =======
-
-// @Module({
-//   imports:[ServeStaticModule.forRoot({rootPath:join(__dirname,"..","app"),
-// }),TypeOrmModule.forRoot(),AccesoriosModule, TramoModule],
-//   controllers: [AppController],
-//   providers: [AppService],
-// >>>>>>> master
+}),TypeOrmModule.forRoot(),AccesoriosModule, ArtefactoModule, TramoModule, UsuarioModule, TramoaccesorioModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
 
