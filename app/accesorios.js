a928ff5434d6fc0d6c06a3e3685b53f5878ebb9d
marianela-miguel3 <nelaecopol@gmail.com>
2022-01-23 15:38:21 -0300

    cambios 23enero

--- app/accesorios.js
+++ app/accesorios.js
@@ -1,31 +1,45 @@
-let cargando=document.getElementById(`cargando`);
-let container=document.getElementById(`container`);
-let accesorios=[];
-let idTramo=document.getElementById(`idTramo`);
-let accesorio=document.getElementById(`accesorio`);
-let cantidad=document.getElementById(`cantidad`);
-let sumaEquivalente=document.getElementById(`sumaEquivalente`);
+let idTramo=document.getElementById(`idTramo`);//input
+let comienzoTramo=document.getElementById(`comienzoTramo`);//input
+let finTramo=document.getElementById(`finTramo`);//input
+let longitudReal=document.getElementById(`longitudReal`);//input
+let longitudCalculo=document.getElementById(`longitudCalculo`);//input
+let calorias=document.getElementById(`calorias`);//input
+//let cargando=document.getElementById(`cargando`);//
+let cargando2=document.getElementById(`cargando2`);//mensaje de carga los accesorios
+//hasta aca
+let mostrarError=document.getElementById(`mostrarError`);//mensaje de carga de tramosacessorios
+let container=document.getElementById(`container`);//muestra los accesorios[]
+let recarga=document.getElementById(`recarga`);//mensaje de carga de tramos
+// let idTramo=document.getElementById(`idTramo`);
+let accesorio=document.getElementById(`accesorio`);//input
+let cantidad=document.getElementById(`cantidad`);//input
+// let sumaEquivalente=document.getElementById(`sumaEquivalente`);//??
 let precio=document.getElementById(`precio`);
-let cargar=document.getElementById(`cargar`);
-let mostrar=document.getElementById(`mostrar`);
-let calcularEquivalente=document.getElementById(`calcularEquivalente`);
+let cargar=document.getElementById(`cargar`);//boton cargar los accesorios
+let mostrar=document.getElementById(`mostrar`);//muestra los tramosaccesorios
+let cargarTramo=document.getElementById(`cargarTramo`);//boton para agregar un tramo
+let mostrarTA=document.getElementById(`mostrarTA`);//mostrar los tramos
+let accesorios=[];
 let tramosAccesorios=[];
-load();
-loadTramoAccesorio();
+let tramos=[];
+let tramosAccesoriosCargados=[];
 
-async function load() {
-    cargando.innerHTML = `<h1>Loading.....</h1>`;
+// loadTramoAccesorio();
+loadAccesorio()
+ 
+async function loadAccesorio() {
+    cargando2.innerHTML = `<h1>Loading.....</h1>`;
     try {
       let response = await fetch(`/accesorios`);
       if (response.ok) {
         accesorios = await response.json();
-        console.log(accesorios);
+        // console.log(accesorios);
         // accesorios = t;
         actualizarListado();
-        cargando.innerHTML = '';
-      } else cargando.innerHTML = `<h1>Error=Failed URL</h1>`;
+        cargando2.innerHTML = '';
+      } else cargando2.innerHTML = `<h1>Error=Failed URL</h1>`;
     } catch (err) {
-      cargando.innerHTML = `<h1> ${err.message} </h1>`;
+      cargando2.innerHTML = `<h1> ${err.message} </h1>`;
     }
   }
 
@@ -44,18 +58,17 @@ async function load() {
     container.innerHTML = html;
   }
 
-  async function loadTramoAccesorio() {
-    // mostrar.innerHTML = `<h1>Loading.....</h1>`;
+  async function loadTramoAccesorio() {//LOAD TRAMOACCESORIO
+    //mostrarError.innerHTML = `<h1>Loading.....</h1>`;
     try {
-      let response = await fetch(`/tramoaccesorio`);
+      let response = await fetch(`/tramoaccesorio`);//me los busca
       if (response.ok) {
-        tramosAccesorios = await response.json();
-        actualizarTramoAccesorio();
-        // cargarEquivalenteTotal();
-        // cargando.innerHTML = '';
-      } else mostrar.innerHTML = `<h1>Error=Failed URL</h1>`;
+        tramosAccesorios = await response.json();//me los guarda en tramosAccesorios
+        //console.log(tramosAccesorios);
+         actualizarTramoAccesorio();
+      } else mostrarError.innerHTML = `<h1>Error=Failed URL</h1>`;
     } catch (err) {
-      mostrar.innerHTML = `<h1> ${err.message} </h1>`;
+      mostrarError.innerHTML = `<h1> ${err.message} </h1>`;
     }
   }
 
@@ -76,15 +89,15 @@ function cargarPrecio(){
   }
   cargar.addEventListener('click',()=>{
     let tramoAccesorio={
-        "idTramo": parseInt(idTramo.value),
+        // "idTramo": parseInt(idTramo.value),
         "idAccesorio": parseInt(accesorio.value),
         "cantidad":parseInt(cantidad.value),
-        "equivalenteTramo":(cargarEquivalente()*cantidad.value).toFixed(2),
+        "equivalenteTramo":parseFloat(cargarEquivalente()*cantidad.value).toFixed(2),
         "tramo_precio_accesorio":cargarPrecio()*cantidad.value
     };
     crearTramoAccesorio(tramoAccesorio);
-    tramosAccesorios.push(tramoAccesorio);
-    actualizarTramoAccesorio();
+    tramosAccesoriosCargados.push(tramoAccesorio);
+    actualizarTramoAccesorioCargado();
 });
 
 
@@ -98,32 +111,119 @@ function cargarPrecio(){
       body:JSON.stringify(tramoAccesorio)
    });
    let r=await response.json();
-   console.log(r)
+  //  console.log(r)
   }
 
-  function actualizarTramoAccesorio() {
+  async function actualizarTramoAccesorioCargado() {
     html = '';
-    for (let i = 0; i < tramosAccesorios.length; i++) {
+    for (let i = 0; i <tramosAccesoriosCargados.length; i++) {
       html += `
                  <tr>
-                     <td>${tramosAccesorios[i].idTramoAccesorio}</td>
-                     <td>${tramosAccesorios[i].idTramo}</td>
-                     <td>${tramosAccesorios[i].idAccesorio}</td>
-                     <td>${tramosAccesorios[i].cantidad}</td>
-                     <td>${tramosAccesorios[i].equivalenteTramo}</td>
-                     <td>${tramosAccesorios[i].tramo_precio_accesorio}</td>
+                     <td>${tramosAccesoriosCargados[i].idTramoAccesorio}</td>
+                     <td>${tramosAccesoriosCargados[i].idAccesorio}</td>
+                     <td>${tramosAccesoriosCargados[i].cantidad}</td>
+                     <td>${tramosAccesoriosCargados[i].equivalenteTramo}</td>
+                     <td>${tramosAccesoriosCargados[i].tramo_precio_accesorio}</td>
                      </tr>`;
     }
     mostrar.innerHTML = html;
   }
 
-  calcularEquivalente.addEventListener('click',()=>{
-    let mostrarCalculo=document.getElementById(`mostrarCalculo`);
-      let total=0;
-      for(let i=0; i<tramosAccesorios.length; i++){
-        if(tramosAccesorios[i].idTramo==idTramo.value){
-          total+=tramosAccesorios[i].equivalenteTramo;
+  // async function actualizarTramoAccesorio() {
+  //   html = '';
+  //   for (let i = 0; i <tramosAccesorios.length; i++) {
+  //     html += `
+  //                <tr>
+  //                    <td>${tramosAccesorios[i].idTramoAccesorio}</td>
+  //                    <td>${tramosAccesorios[i].idAccesorio}</td>
+  //                    <td>${tramosAccesorios[i].cantidad}</td>
+  //                    <td>${tramosAccesorios[i].equivalenteTramo}</td>
+  //                    <td>${tramosAccesorios[i].tramo_precio_accesorio}</td>
+  //                    </tr>`;
+  //   }
+  //   mostrar.innerHTML = html;
+  // }
+
+  cargarTramo.addEventListener('click',()=>{
+    let tramo={
+      "nombre_tramo": `${comienzoTramo.value} ${finTramo.value}`,
+      "longitud_real": longitudReal.value,
+      "longitud_de_calculo":longitudCalculo.value,
+      "equivalente_total":calcularEquivalenteTotal(),
+      "total":(calcularEquivalenteTotal() + parseInt(longitudCalculo.value)),
+      "metros_cubicos":(calorias.value/9300).toFixed(2)
+  };
+  tramos.push(tramo);
+  crearTramo(tramo);
+  actualizarTramo();
+  tramosAccesoriosCargados=[];
+  idTramo.value=" ";
+});
+
+
+async function calcularEquivalenteTotal(){
+  let total=0;
+      for(let i=0;i<tramosAccesoriosCargados.length;i++){
+        total+=tramosAccesoriosCargados[i].equivalenteTramo;
         }
-        mostrarCalculo.innerHTML=parseInt(total);
-      }
-  });
+        console.log(total);
+      return total;
+} 
+
+
+async function crearTramo(tramo){
+let response= await fetch("/tramos",{
+  method: "POST",
+  headers:{
+    "Content-Type":"application/json",
+  },
+  body:JSON.stringify(tramo)
+});
+let r=await response.json();
+}
+
+  // async function loadTramoAccesorio() {
+  //   cargando.innerHTML = `<h1>Loading.....</h1>`;
+  //   try {
+  //     let response = await fetch(`/tramoaccesorio`);
+  //     if (response.ok) {
+  //       tramosAccesorios = await response.json();
+  //       // console.log(tramosAccesorios);
+  //       cargando.innerHTML = '';
+  //     } else cargando.innerHTML = `<h1>Error=Failed URL</h1>`;
+  //   } catch (err) {
+  //     cargando.innerHTML = `<h1> ${err.message} </h1>`;
+  //   }
+  // }
+
+  async function loadTramo() {
+    recarga.innerHTML = `<h1>Loading.....</h1>`;
+    try {
+      let response = await fetch(`/tramos`);
+      if (response.ok) {
+        tramos = await response.json();
+        actualizarTramo();
+        recarga.innerHTML = '';
+      } else recarga.innerHTML = `<h1>Error=Failed URL</h1>`;
+    } catch (err) {
+      recarga.innerHTML = `<h1> ${err.message} </h1>`;
+    }
+  }
+
+  async function actualizarTramo() {
+    html = ``;
+    for (let i = 0; i < tramos.length; i++) {
+      html += `
+                 <tr>
+                     <td>${tramos[i].idTramo}</td>
+                     <td>${tramos[i].nombre_tramo}</td>
+                     <td>${tramos[i].longitud_real}</td>
+                     <td>${tramos[i].longitud_de_calculo}</td>
+                     <td>${tramos[i].equivalente_total}</td>
+                     <td>${tramos[i].total}</td>
+                     <td>${tramos[i].metros_cubicos}</td>
+                     </tr>`;
+    }
+    mostrarTA.innerHTML = html;
+    // container.insertColumn(-1).innerHTML = '<tr><td>'+artefacto.value+'</td>'
+}
--- src/tramo/tramo.entity.ts
+++ src/tramo/tramo.entity.ts
@@ -13,21 +13,42 @@ export default class Tramo{
     @Column()
     private longitud_de_calculo:number;
     @Column()
+    private equivalente_total:number;
+    @Column()
+    private total:number;
+    @Column()
     private metros_cubicos:number;
+    @Column()
+    private diametro_de_calculo:number;
+    @Column()
+    private diametro_adoptado:number;
 
     @OneToMany(type => TramoAccesorio,tramoAccesorios => tramoAccesorios.tramo,{
         cascade: true,
     })
     public tramoAccesorios:TramoAccesorio[];
 
-    public constructor(nombre_tramo:string,longitud_real:number,longitud_de_calculo:number,metros_cubicos:number){    
+    public constructor(nombre_tramo:string,longitud_real:number,longitud_de_calculo:number,equivalente_total:number, total:number,metros_cubicos:number,diametro_de_calculo:number,diametro_adoptado:number){    
         this.nombre_tramo=nombre_tramo;
         this.longitud_real=longitud_real;
         this.longitud_de_calculo=longitud_de_calculo;
+        this.equivalente_total=equivalente_total;
+        this.total=total;
         this.metros_cubicos=metros_cubicos;
+        this.diametro_de_calculo=diametro_de_calculo;
+        this.diametro_adoptado=diametro_adoptado;
     }
     public getIdTramo():number{
         return this.idTramo;
+    }
+    public setEquivalenteTotal(equiNuevo:number):void{
+        this.equivalente_total=equiNuevo;
+    }
+    public setTotal(totalNuevo:number):void{
+        this.total=totalNuevo;
+    }
+    public setDiametroAdoptado(diametroNuevo:number):void{
+        this.diametro_adoptado=diametroNuevo;
     }
 
     public setNombre_tramo(nombre_tramo:string){
